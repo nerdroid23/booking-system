7b7ebd435f93268ee42bbabf97687a453bd310cd
joe <nerdroid23@gmail.com>
2025-08-29 17:06:15 -0400

    Update the checkout process to accept the start date from request and adjust availability fetching

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -11,25 +11,33 @@ use App\Models\Employee;
 use App\Models\Service;
 use App\Services\Bookings\DateCollection;
 use App\Services\Bookings\ServiceSlotAvailability;
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Inertia\Response;
 
 class CheckoutController extends Controller
 {
-    public function __invoke(Service $service, ?Employee $employee = null): Response
+    public function __invoke(Request $request, Service $service, ?Employee $employee = null): Response
     {
         $employees = $employee ? collect([$employee]) : $service->employees;
 
         /** @var DateCollection $availability */
         $availability = new ServiceSlotAvailability(employees: $employees, service: $service)
-            ->forPeriod(start: now()->startOfDay(), end: now()->addMonth()->endOfDay())
+            ->forPeriod(
+                start: Carbon::createFromDate($request->query('start')),
+                end: Carbon::createFromDate($request->query('start'))->endOfMonth()
+            )
             ->hasSlots()
             ->values();
 
         return inertia()->render('checkout/Index', [
             'service' => ServiceData::from($service),
             'employee' => $employee ? EmployeeData::from($employee) : null,
+            // TODO: Use deferred
+            // 'availability' => inertia()->defer(fn () => AvailabilityData::collect($availability)),
             'availability' => AvailabilityData::collect($availability),
             'date' => $availability->firstAvailableDate()?->date->toDateString(),
+            'start' => $request->query('start'),
         ]);
     }
 }
--- resources/js/pages/checkout/Index.vue
+++ resources/js/pages/checkout/Index.vue
@@ -3,23 +3,38 @@ import Layout from '@/layouts/HomeLayout.vue';
 import { Availability, Employee, Service } from '@/types/generated';
 import { easepick, LockPlugin } from '@easepick/bundle';
 import styles from '@easepick/bundle/dist/index.css?url';
-import { Head, Link } from '@inertiajs/vue3';
+import { type Core } from '@easepick/core/dist/core';
+import { Head, Link, router } from '@inertiajs/vue3';
 import { onMounted, ref } from 'vue';
 
 defineOptions({ layout: Layout });
 
 const props = defineProps<{
+  // TODO: use deferred prop
   availability: Array<Availability>;
   employee: Employee | null;
   service: Service;
   date: string | null;
+  start: string | null;
 }>();
 
-let picker = null;
+let picker: Core | null = null;
 const pickerRef = ref<HTMLElement | null>(null);
 
 onMounted(() => {
   createPicker();
+
+  picker?.on('render', (event: CustomEvent) => {
+    if (event.detail.view === 'Container' && event.detail.date.format('YYYY-MM-DD').toString() !== props.start) {
+      router.reload({
+        data: { start: event.detail.date.format('YYYY-MM-DD') },
+        only: ['availability', 'start', 'date'],
+        onSuccess() {
+          picker?.renderAll();
+        },
+      });
+    }
+  });
 });
 
 const createPicker = () => {
@@ -37,7 +52,7 @@ const createPicker = () => {
           return true;
         }
 
-        return !props.availability.find((a) => a.date === date.format('YYYY-MM-DD'));
+        return !props.availability?.find((a) => a.date === date.format('YYYY-MM-DD'));
       },
     },
   });
