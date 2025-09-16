c701195fc8cc53c9ee9fe17cc9cda8dee108ad8c
joe <nerdroid23@gmail.com>
2025-09-16 13:27:27 -0400

    feat: introduce DayOfWeek enum and refactor working hours retrieval

--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use App\Enums\DayOfWeek;
 use Carbon\CarbonImmutable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -31,13 +32,13 @@ class Schedule extends Model
     public function getWorkingHoursForDate(CarbonImmutable $date): ?array
     {
         $hours = match ($date->dayOfWeek) {
-            0 => [$this->sunday_starts_at, $this->sunday_ends_at],
-            1 => [$this->monday_starts_at, $this->monday_ends_at],
-            2 => [$this->tuesday_starts_at, $this->tuesday_ends_at],
-            3 => [$this->wednesday_starts_at, $this->wednesday_ends_at],
-            4 => [$this->thursday_starts_at, $this->thursday_ends_at],
-            5 => [$this->friday_starts_at, $this->friday_ends_at],
-            6 => [$this->saturday_starts_at, $this->saturday_ends_at],
+            DayOfWeek::Sunday->value => [$this->sunday_starts_at, $this->sunday_ends_at],
+            DayOfWeek::Monday->value => [$this->monday_starts_at, $this->monday_ends_at],
+            DayOfWeek::Tuesday->value => [$this->tuesday_starts_at, $this->tuesday_ends_at],
+            DayOfWeek::Wednesday->value => [$this->wednesday_starts_at, $this->wednesday_ends_at],
+            DayOfWeek::Thursday->value => [$this->thursday_starts_at, $this->thursday_ends_at],
+            DayOfWeek::Friday->value => [$this->friday_starts_at, $this->friday_ends_at],
+            DayOfWeek::Saturday->value => [$this->saturday_starts_at, $this->saturday_ends_at],
             default => [null, null],
         };
 
